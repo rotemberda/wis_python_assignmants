d769af34f78916949f86294b31345b88ac8c08b0
rotemberda <rotemberda@gmail.com>
2024-11-24 19:35:02 +0200

    guessing_game.py aploaded to day04/ folder

---
+++ day04/guessing_game.py
@@ -0,0 +1,93 @@
+from random import randint
+import sys
+
+
+def main():
+    # print a message to the user about the start of the game
+    print("Welcome to the guessing game!!")
+    
+    # start a round
+    while True:
+        game_round()
+
+        # ask if user wants another round
+        another_round = input("That was fun:) \nDo you want to play another round? (Yes/No) ").lower()
+
+        if another_round == 'yes' or another_round == 'y':
+            continue
+        
+        else:
+            print('\n' * 10, "See you next time :)", 'Quiting game...', sep = "\n")
+            break
+
+
+
+
+def game_round():                # starts a game round
+
+    # generate a random number
+    global secret_number
+    secret_number = randint(1, 20)
+
+    guess_num = 0
+   
+    while True:
+        # ask for the user's guess
+        guess = prompt_guess()
+        
+        if guess == 'n':
+            print("Ending round.")
+            break
+    
+        # compare to the secret number and inform the user
+        if guess == secret_number:
+            print("You Won!", f"You needed {guess_num} guesses.", sep = "\n")
+            break
+
+        else:
+            print(check_value(guess))
+            guess_num += 1
+
+    
+
+
+def prompt_guess():                        # ask for user's guess and check if valid
+    
+    while True:
+        guess = input("What's your guess? ")
+        
+    #special inputs
+        if guess.lower() == 'x':
+            print('\n' * 10, "See you next time :)", 'Quiting game...', sep = "\n")
+            sys.exit(0)
+
+        if guess == 'n':
+            return guess.lower()
+        
+        elif guess.lower() == 's':
+            print("I usually do not condone cheating, but it seems this is a tough one...", f"the secrete number is {secret_number}", sep = "\n" * 2, end = '\n' * 2)
+            return prompt_guess()
+        
+    # validating the user's input
+        try:
+            return int(guess)
+            break
+        except:
+            print("Invalid input! Input type isn't 'int'")
+    
+
+    
+
+
+def check_value(guess):     # check wether the user's guess is too small or too big    
+    
+    if guess < secret_number:
+        return "Your guess is too small! Try again."
+    else:
+        return "Your guess is too big! Try again."
+    
+
+
+
+if __name__ == '__main__':
+    main()  
