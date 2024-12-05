24505c4f3f3649b96eb8eba2cee7b6f39b0f702e
rotemberda <rotemberda@gmail.com>
2024-12-05 14:57:57 +0200

    day05 assignment

---
+++ day05/seq.py
@@ -0,0 +1,95 @@
+import sys
+import os.path
+
+def main():
+    # validate the argv inputs
+    argv = sys.argv
+
+    validate_input(argv)
+    
+    all = []
+    dict_all = {"A": 0, "T": 0, "C": 0, "G": 0, "Unknown": 0, "Total": 0}
+
+    # go over each file given and output the results
+    for path in argv[1:]:
+        # extraction the seq from each file
+        seq = read_file(path)
+
+        # saving the analysis results in a list
+        all.append(analyze_seq(seq))
+
+    for i, result_dict in enumerate(all):
+        # printing the file name
+        print(argv[i + 1])
+
+        # printing the analysis results
+        print_result(result_dict)
+
+        # updating the all the results to a single dict
+        for nuc,count in result_dict.items():
+             dict_all[nuc] += count
+    
+    # printing the analysis of all the files combined
+    print("All")
+    print_result(dict_all)
+
+
+def validate_input(argv):
+    # check for file paths
+    if len(argv) < 2:
+        exit("Usage: python seq.py filepath1.txt filepath2.txt")
+
+    # check that all files are .txt files
+    for path in argv[1:]:
+        if path[-4:] != '.txt':
+            exit(f"Usage: '{path}' is not a .txt file")
+    
+        # validate that the file exists
+        if not os.path.isfile(path):
+            exit(f"'{path}' doesn't exists")
+
+    # if passed all tests return True
+    return True
+
+
+def read_file(path):
+    # read the file and return the sequence
+    with open(path, 'r') as file:
+        seq = file.read()
+    return seq[:-1]
+
+
+def analyze_seq(seq):
+    # count the number of A, T, C, G and unknown and returns a dictionary
+
+    # making the analyze case insensative
+    seq = seq.upper()
+
+    result = {}
+    nuc_num = 0
+
+    # counting each nuclaotide and saving its count to result
+    for nuc in ["A", "T", "C", "G"]:
+        count = seq.count(nuc)
+        result[nuc] = count
+        nuc_num += count
+
+    # adding to result any unknown charactors
+    result["Unknown"] = len(seq) - nuc_num
+    result["Total"] = len(seq)
+
+    # return a dict of the countings
+    return result
+
+
+def print_result(result):
+    for nuc, count in result.items():
+        if nuc == "Total":
+            continue
+        print(f"{nuc}:{' ' * (8 - len(nuc))} {count} {round(count / result["Total"] * 100, 1)}%")
+    
+    print(f"Total:{' ' * 3} {result["Total"]}\n\n")
+
+
+if __name__ == '__main__':
+    main()
---
+++ day05/test_seq.py
@@ -0,0 +1,43 @@
+from seq import validate_input, read_file, analyze_seq, print_result
+import pytest 
+
+def test_validate_input():
+    # valid input
+    assert validate_input(["seq.py", "a_seq.txt"]) == True
+
+    # no file path were given
+    with pytest.raises(SystemExit) as excinfo:
+        validate_input(["seq.py"])
+
+    assert excinfo.value.code == "Usage: python seq.py filepath1.txt filepath2.txt"
+
+    # some files aren't .txt files
+    with pytest.raises(SystemExit) as excinfo:
+        validate_input(["seq.py", "invalid"])
+
+    assert excinfo.value.code == "Usage: 'invalid' is not a .txt file"
+    
+    with pytest.raises(SystemExit) as excinfo:
+        validate_input(["seq.py", "a_seq.txt", "invalid"])
+
+    assert excinfo.value.code == "Usage: 'invalid' is not a .txt file"
+
+    # some files don't exist
+    with pytest.raises(SystemExit) as excinfo:
+        validate_input(["seq.py", "invalid.txt"])
+
+    assert excinfo.value.code == "'invalid.txt' doesn't exists"
+
+    with pytest.raises(SystemExit) as excinfo:
+        validate_input(["seq.py","a_seq.txt", "invalid.txt"])
+
+    assert excinfo.value.code == "'invalid.txt' doesn't exists"
+
+def test_read_file():
+    assert read_file("a_seq.txt") == "ACCTGXXCXXGTTACTGGGCXTTGTXX"
+    assert read_file("b_seq.txt") == "ACCGGGTTTT"
+
+
+def test_analyze_seq():
+    assert analyze_seq("abcdT") == {"A": 1, "T": 1, "C": 1, "G": 0, "Unknown": 2, "Total": 5}
+    assert analyze_seq("aAAtTcCgGh") == {"A": 3, "T": 2, "C": 2, "G": 2, "Unknown": 1, "Total": 10}
